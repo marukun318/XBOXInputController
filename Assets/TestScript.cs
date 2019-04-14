9a991cfc6ec20606fa60b7d3b0786cbeb809bb4e
Takeshi Maruyama <maru@marukun.jp>
2019-04-15 02:07:55 +0900

    Implement actual usage examples in "TestScript.cs".

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -1,23 +1,24 @@
-﻿// These codes are licensed under CC0.
-// http://creativecommons.org/publicdomain/zero/1.0/deed.ja
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
+    [SerializeField]
     private XBOXInputController inputController;
 
     // Start is called before the first frame update
     IEnumerator Start()
     {
-#if NET_2_0 || NET_2_0_SUBSET || NET_STANDARD_2_0
-#error Set .NET Version to 4.6 or later
-#endif
-        inputController = XBOXInputController.Instance;
 
         yield break;
     }
-  
+
+    // Update is called once per frame
+    void Update()
+    {
+        uint pad = inputController.Poll();      // パッド入力
+
+        
+    }
 }
